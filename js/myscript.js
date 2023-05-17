0b75260c5c9dd5e6e9254b8eb9d415d299842611
rc2pc2 <petriccarcc@gmail.com>
2023-05-17 10:11:39 +0200

    removed guideline comments

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,3 @@
-/**
- *
- * Scrivi un programma che stampi in console i numeri da 1 a 100,
- *
- * ma che per i multipli di 3 stampi “Fizz” al posto del numero e
- * per i multipli di 5 stampi “Buzz”.
- *
-Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
- */
 
 const ulElement = document.querySelector('ul');
 
@@ -29,7 +20,6 @@ for (let i = 1 ; i <= 100 ; i++){
         liElement.innerHTML = '<h3>Buzz</h3>';
         console.log('Buzz');
         liElement.classList.add('buzz');
-
     } else if ( (i % 3 === 0)){
         liElement.innerHTML = '<h3>Fizz</h3>';
         console.log('Fizz');
@@ -41,6 +31,5 @@ for (let i = 1 ; i <= 100 ; i++){
     }
 
     liElement.appendChild(newElement);
-
     ulElement.appendChild(liElement);
 }
