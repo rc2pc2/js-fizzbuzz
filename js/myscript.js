5fde72d425839462bdd28fb6f8ebe1f1dea9aab3
rc2pc2 <petriccarcc@gmail.com>
2023-05-17 10:29:53 +0200

    refactoring on both html and script

--- js/myscript.js
+++ js/myscript.js
@@ -1,9 +1,7 @@
 
 const ulElement = document.querySelector('ul');
 
-// ? i++ => i = i + 1;
 for (let i = 1 ; i <= 100 ; i++){
-
     const liElement = document.createElement('li');
     liElement.classList.add('box');
 
@@ -12,21 +10,20 @@ for (let i = 1 ; i <= 100 ; i++){
     newElement.classList.add('class-wrapper');
 
     if ( i % 15 === 0 ) {
-        console.log('FizzBuzz');
+        // console.log('FizzBuzz');
         liElement.innerHTML = '<h3>FizzBuzz</h3>';
         liElement.classList.add('fizzbuzz');
     } else if ( i % 5 === 0 ){
+        // console.log('Buzz');
         liElement.innerHTML = '<h3>Buzz</h3>';
-        console.log('Buzz');
         liElement.classList.add('buzz');
     } else if ( (i % 3 === 0)){
+        // console.log('Fizz');
         liElement.innerHTML = '<h3>Fizz</h3>';
-        console.log('Fizz');
         liElement.classList.add('fizz');
-
     } else {
+        // console.log(i);
         liElement.innerHTML = '<h3>' + i + '</h3>';
-        console.log(i);
     }
 
     liElement.appendChild(newElement);
